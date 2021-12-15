d0f3282515ac54fc7927134e7d2b8aea5336a2dd
leprechaunni <marckevi4vikk@yandex.ru>
2021-12-16 00:31:39 +0300

    - 30 module

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
 const express = require('express');
-const path = require('path')
-const exphbs = require('express-handlebars')
+const path = require('path');
+const exphbs = require('express-handlebars');
+const homeRoutes = require('./routes/home')
+const addRoutes = require('./routes/add')
+const coursesRoutes = require('./routes/courses')
 
-const app = express() 
+const app = express();
 
 const hbs = exphbs.create({
    defaultLayout: 'main',
    extname: 'hbs',
-})
+});
 
-app.engine('hbs', hbs.engine) //регистрируем данный движок
-app.set('view engine', 'hbs') //начинаем использовать
-app.set('views', 'views') //название папки, где будут зраниться шаблоны
+app.engine('hbs', hbs.engine); //регистрируем данный движок
+app.set('view engine', 'hbs'); //начинаем использовать
+app.set('views', 'views'); //название папки, где будут зраниться шаблоны
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+app.use(express.urlencoded({extended: true}))
+
+app.use('/', homeRoutes);
+app.use('/add', addRoutes);
+app.use('/courses', coursesRoutes);
+
+// app.get('/', (req, res) => {
+//    //res.status(200) идет по умолчанию
+//    //res.sendFile(path.join(__dirname, 'views', 'index.html')) убрали, т.к. для файлов .hbs она нам больше не нужна
+//    res.render('index', {
+//       title: 'Главная страница',
+//       isHome: true,
+//    });
+//});
 
 
-app.get('/', (req, res) => {
-   //res.status(200) идет по умолчанию
-   //res.sendFile(path.join(__dirname, 'views', 'index.html')) убрали, т.к. для файлов .hbs она нам больше не нужна
-   res.render('index')
-})
-app.get('/about', (req, res) => {
-   res.render('about')
-})
-
-
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-   console.log(`Server is running on port ${PORT}`)
-})
+   console.log(`Server is running on port ${PORT}`);
+});
